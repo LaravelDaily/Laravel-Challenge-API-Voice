8fededbc3c384c3db28e7f84ce46443e934be7be
wolf <wolf@aptrix.co.in>
2021-07-30 12:49:10 +0530

    My Version Code

--- VoiceController.php
+++ VoiceController.php
@@ -1,48 +1,45 @@
-public function voice(Request $request){
-    $request->validate([
-        'question_id'=>'required|int|exists:questions,id',
-        'value'=>'required|boolean',
-    ]);
+<?php
 
-    $question=Question::find($request->post('question_id'));
-    if (!$question)
-        return response()->json([
-            'status'=>404,
-            'message'=>'not found question ..'
-        ]);
-    if ($question->user_id==auth()->id())
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote to your question'
-        ]);
+namespace App\Http\Controllers;
 
-    //check if user voted 
-    $voice=Voice::where([
-        ['user_id','=',auth()->id()],
-        ['question_id','=',$request->post('question_id')]
-    ])->first();
-    if (!is_null($voice)&&$voice->value===$request->post('value')) {
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote more than once'
-        ]);
-    }else if (!is_null($voice)&&$voice->value!==$request->post('value')){
-        $voice->update([
-            'value'=>$request->post('value')
-        ]);
-        return response()->json([
-            'status'=>201,
-            'message'=>'update your voice'
-        ]);
+use Illuminate\Http\Request;
+
+class VocieController extends Controller
+{
+
+    protected $rules = [
+        'question_id' => 'required|int|exists:questions,id',
+        'value' => 'required|boolean',
+    ];
+
+    public function voice(Request $request)
+    {
+        // if not validate return
+        $request->validate($this->rules);
+
+        // 404 if Question not find with where , so The user is not allowed to vote to your question
+        $question = Question::where('user_id', auth()->id())->firstOrFail($request->question_id);
+
+        $responseData = [
+            'status' => 200,
+            'message' => 'Voting completed successfully',
+        ];
+
+        $matchThese = [
+            'value' => $request->value,
+            'user_id' => auth()->id(),
+        ];
+        $voice = $question->voice()->firstOrCreate($matchThese, ['value' => $request->value]);
+
+        // this if condition response is meaningless
+        if ($voice->value === $request->value) {
+            $responseData['status'] = 500;
+            $responseData['message'] = 'The user is not allowed to vote more than once';
+        } else {
+            $question->voice()->updateOrCreate($matchThese, ['value' => $request->value]);
+        }
+
+        return response()->json($responseData, $responseData['status']);
     }
 
-    $question->voice()->create([
-        'user_id'=>auth()->id(),
-        'value'=>$request->post('value')
-    ]);
-
-    return response()->json([
-        'status'=>200,
-        'message'=>'Voting completed successfully'
-    ]);
 }
