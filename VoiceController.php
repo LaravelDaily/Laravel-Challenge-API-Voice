5062d998f9f10af0fb452b9a272544875ca55023
Mark Snape <mark@novate.co.uk>
2021-07-30 13:33:44 +0100

    checked logic, user can't voice their own question

--- VoiceController.php
+++ VoiceController.php
@@ -1,10 +1,12 @@
 public function voice(Request $request){
 
-    $question = Auth::user()->questions()->findOrFail($request->question_id));
+    // can't vote on own question
+    $question = Question::where('user_id','!=',Auth::id())->findOrFail($request->question_id));
 
+    // assumes there is a relationship of a user's voices
     Auth::user()->voices()->createOrUpdate([
         'question_id' => $question->id,
-        'value' => !! $request->value
+        'value' => !! $request->value  // no need to validate just coerce to boolean, ideally change name
     ]);
 
     return response()->json([
