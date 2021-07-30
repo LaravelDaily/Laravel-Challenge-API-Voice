13cdcc4e22a3d815d811e044086a11a7511a12d4
gustiawan <agusx244@gmail.com>
2021-07-30 20:10:15 +0700

    remove comment

--- VoiceController.php
+++ VoiceController.php
@@ -13,17 +13,14 @@ class VoiceController extends Controller
     {
         $question = Question::with('voices')->find($request->post('question_id'));
 
-        // return not found even if the question is belongs the user id
         if (!$question) {
             return $this->sendErrorResponse("Question not found.", 404);
         }
 
-        // using gate to prevent update with rules
         if (!Gate::inspect('update', $question)->allowed()) {
             return $this->sendErrorResponse("You cant vote to your own question", 403);
         }
 
-        // check if user voted 
         $voice = $question->voices()->where('user_id', auth()->id())->first();
 
         if ($voice) {
@@ -35,10 +32,7 @@ class VoiceController extends Controller
             return $this->sendJsonResponse(['message' => 'Your vote has been updated.'], 201);
         }
 
-        // not sure if you want to change relation name. 
-        // but i do want to change it to voices since it must be one to many relationship.
         $question->voices()->create($request->only('user_id', 'value'));
-
-        return $this->sendJsonResponse(['message' => 'Your vote has been submitted.']);
+        return $this->sendJsonResponse(['message' => 'Your vote has been submited.']);
     }
 }
