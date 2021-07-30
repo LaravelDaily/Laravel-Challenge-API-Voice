a128de6765d9c7ac0f4a2c33004c54812c3181dd
Mark Snape <mark@novate.co.uk>
2021-07-30 13:43:55 +0100

    Update VoiceController.php

--- VoiceController.php
+++ VoiceController.php
@@ -1,6 +1,7 @@
-public function voice(Request $request){
+public function voice(Request $request)
+{
 
-    // can't vote on own question
+    // can't voice on own question
     $question = Question::where('user_id','<>',Auth::id())->findOrFail($request->question_id));
 
     // assumes there is a relationship of a user's voices
@@ -10,7 +11,26 @@ public function voice(Request $request){
     );
 
     return response()->json([
-        'status'=>200,
-        'message'=>'Voting recorded successfully'
+        'status' => 200,
+        'message' => 'Voting recorded successfully'
+    ]);
+}
+
+// version using route model binding 
+public function voice(Request $request, Question $question)
+{
+
+    // can't voice on own question
+    abort_if($question->user_id == Auth::id()),403,'You cannot voice your own question');
+
+    // assumes there is a relationship of a user's voices
+    Auth::user()->voices()->createOrUpdate(
+        ['question_id' => $question->id],
+        ['value' => !! $request->value]     // no need to validate just coerce to boolean, ideally change name
+    );
+
+    return response()->json([
+        'status' => 200,
+        'message' => 'Voting recorded successfully'
     ]);
 }
