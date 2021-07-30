52541bd4de1817a41dc9c29f4d68189acd182d7b
shuvomohajan <shuvomohajan@gmail.com>
2021-07-30 12:54:39 +0600

    Voice Controller Api Correction wite Form Request

--- VoiceController.php
+++ VoiceController.php
@@ -1,48 +1,36 @@
-public function voice(Request $request){
-    $request->validate([
-        'question_id'=>'required|int|exists:questions,id',
-        'value'=>'required|boolean',
-    ]);
-
+public function voice(VoiceRequest $request){
     $question=Question::find($request->post('question_id'));
     if (!$question)
         return response()->json([
-            'status'=>404,
             'message'=>'not found question ..'
-        ]);
+        ], 404);
     if ($question->user_id==auth()->id())
         return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote to your question'
-        ]);
+            'message'=>'The user is not allowed to vote to your question'
+        ], 500);
 
-    //check if user voted 
+    //check if user voted
     $voice=Voice::where([
-        ['user_id','=',auth()->id()],
-        ['question_id','=',$request->post('question_id')]
+        'user_id'=>auth()->id(),
+        'question_id'=>$request->post('question_id')
     ])->first();
-    if (!is_null($voice)&&$voice->value===$request->post('value')) {
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote more than once'
-        ]);
-    }else if (!is_null($voice)&&$voice->value!==$request->post('value')){
+
+    if($voice){
         $voice->update([
             'value'=>$request->post('value')
         ]);
+        
         return response()->json([
-            'status'=>201,
-            'message'=>'update your voice'
+            'message'=>'updated successfully'
+        ], 200);
+    }else{
+        $question->voice()->create([
+            'user_id'=>auth()->id(),
+            'value'=>$request->post('value')
         ]);
+
+        return response()->json([
+            'message'=>'Voting completed successfully'
+        ], 200);
     }
-
-    $question->voice()->create([
-        'user_id'=>auth()->id(),
-        'value'=>$request->post('value')
-    ]);
-
-    return response()->json([
-        'status'=>200,
-        'message'=>'Voting completed successfully'
-    ]);
 }
