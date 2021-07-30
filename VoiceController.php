5c6958c2d213277edf251124bb33a9da9a78273f
lamfeng <40447844+lamfeng@users.noreply.github.com>
2021-07-30 21:53:09 +0800

    Voice Code Refactoring

--- VoiceController.php
+++ VoiceController.php
@@ -1,48 +1,68 @@
-public function voice(Request $request){
-    $request->validate([
-        'question_id'=>'required|int|exists:questions,id',
-        'value'=>'required|boolean',
-    ]);
+<?php
 
-    $question=Question::find($request->post('question_id'));
-    if (!$question)
-        return response()->json([
-            'status'=>404,
-            'message'=>'not found question ..'
-        ]);
-    if ($question->user_id==auth()->id())
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote to your question'
-        ]);
+namespace App\Http\Controllers\Voice;
 
-    //check if user voted 
-    $voice=Voice::where([
-        ['user_id','=',auth()->id()],
-        ['question_id','=',$request->post('question_id')]
-    ])->first();
-    if (!is_null($voice)&&$voice->value===$request->post('value')) {
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote more than once'
-        ]);
-    }else if (!is_null($voice)&&$voice->value!==$request->post('value')){
-        $voice->update([
-            'value'=>$request->post('value')
-        ]);
-        return response()->json([
-            'status'=>201,
-            'message'=>'update your voice'
-        ]);
+use Illuminate\Http\Request;
+use  App\Models\Question;
+use  App\Models\Voice;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\VoiceRequest;
+use Exception;
+class VoiceController extends Controller
+{
+    public function voice(VoiceRequest $request)
+    {
+        try {
+            $authId = auth()->id();
+            $question_id = $request->post('question_id');
+            $value = $request->post('value');
+    
+            // Validate in Form Request.
+            // if (!$question) {
+            //     $this->jsonResponse(404, 'not found question ..');
+            // }
+    
+            $question = Question::select('user_id')->where('id', $question_id)->first();
+            if ($question->user_id == $authId) {
+                return $this->jsonResponse(500, 'The user is not allowed to vote to your question');
+            }
+    
+            // Check if user voted 
+            $voice = Voice::select('value')->where([
+                ['user_id', '=', $authId],
+                ['question_id', '=', $question_id]
+            ])->first();
+    
+            if($voice) {
+                if ($voice->value === $value) {
+                    return $this->jsonResponse(403, 'The user is not allowed to vote more than once');
+                } else if ($voice->value !== $value) {
+                    $voice->update([
+                        'value' => $value
+                    ]);
+                    return $this->jsonResponse(201, 'update your voice');
+                } 
+            } else {
+                $question->voice()->create([
+                    'user_id' => $authId,
+                    'value' => $value
+                ]);
+            }
+        } catch (Exception $e) {
+            // var_dump($e->getMessage());
+            return $this->jsonResponse(500, 'Oops... Something Wrong');
+        }
+        
+        return $this->jsonResponse(200, 'Voting completed successfully');
     }
 
-    $question->voice()->create([
-        'user_id'=>auth()->id(),
-        'value'=>$request->post('value')
-    ]);
+    private function jsonResponse($status, $message) {
+        $status = intval($status);
+        $message = strval($message);
 
-    return response()->json([
-        'status'=>200,
-        'message'=>'Voting completed successfully'
-    ]);
+        return response()->json([
+            'status' => $status,
+            'message' => $message
+        ], 200);
+    }
 }
