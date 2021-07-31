9e9adba57e8d61b3273880f51a719c0283aea3e6
Hydrokat <bacaniargieson@gmail.com>
2021-07-31 22:39:35 +0800

    rework

---
+++ VoicePolicy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Policies;
+
+class VoicePolicy
+{
+    use HandlesAuthorization;
+
+    public function vote(User $user, Question $question)
+    {
+        return $question->user_id == $user->id;
+    }
+}
