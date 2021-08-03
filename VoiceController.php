31e1698318657032c48c2512b3b5c9dbeca70311
alireza <alireza.jhd2000@gmail.com>
2021-08-03 18:13:21 +0430

    Clean the code

--- VoiceController.php
+++ VoiceController.php
@@ -1,48 +1,44 @@
-public function voice(Request $request){
-    $request->validate([
-        'question_id'=>'required|int|exists:questions,id',
-        'value'=>'required|boolean',
-    ]);
+<?php
 
-    $question=Question::find($request->post('question_id'));
-    if (!$question)
-        return response()->json([
-            'status'=>404,
-            'message'=>'not found question ..'
-        ]);
-    if ($question->user_id==auth()->id())
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote to your question'
-        ]);
-
-    //check if user voted 
-    $voice=Voice::where([
-        ['user_id','=',auth()->id()],
-        ['question_id','=',$request->post('question_id')]
-    ])->first();
-    if (!is_null($voice)&&$voice->value===$request->post('value')) {
-        return response()->json([
-            'status' => 500,
-            'message' => 'The user is not allowed to vote more than once'
-        ]);
-    }else if (!is_null($voice)&&$voice->value!==$request->post('value')){
-        $voice->update([
-            'value'=>$request->post('value')
-        ]);
-        return response()->json([
-            'status'=>201,
-            'message'=>'update your voice'
-        ]);
+class VoiceController{
+    private static function output($status, $message) {
+        // return the response with status equal to $status and message equal to $message
+        return response()
+        ->json(compact('status', 'message'));
     }
+    public function voice(Request $request){
+        $data = $request->validate([
+            'question_id' => 'required|int|exists:questions,id',
+            'value' => 'required|boolean',
+        ]);
 
-    $question->voice()->create([
-        'user_id'=>auth()->id(),
-        'value'=>$request->post('value')
-    ]);
+        $question = Question::find($data['question_id']);
+        if (!$question)
+            return self::output(404, 'not found question. ');
 
-    return response()->json([
-        'status'=>200,
-        'message'=>'Voting completed successfully'
-    ]);
+        if ($question->user_id == auth()->id())
+            return self::output(500, 'The user is not allowed to vote to your question');
+
+        //check if user voted
+
+        // the question_id of the voice has to be equal to our question's id and equality of user_id and auth()->id checked. so they have an one to one relationship
+        $voice = $question->voice;
+        if(is_null($voice)){
+            $question->voice()->create([
+                'user_id' => auth()->id(),
+                'value' => $data['value']
+            ]);
+
+            return self::output(200, 'Voting completed successfully');
+        }
+        if ($voice->value === $data['value']) {
+            return self::output(500, 'The user is not allowed to vote more than once');
+        }else if ($voice->value !== $data['value']){
+            $voice->update([
+                'value' => $data['value']
+            ]);
+            return self::output(201, 'update your voice');
+        }
+
+    }
 }
